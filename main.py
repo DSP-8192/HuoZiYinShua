79e57c4b9a6b4476f84a1b816a536e39e446dfee
DSP-8192 <70532341+DSP-8192@users.noreply.github.com>
2022-05-06 13:39:37 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from huoZiYinShua import *
 if __name__ == "__main__":
 	HZYS = huoZiYinShua(".\\素材\\", ".\\dictionary.csv")
 	HZYS.export("从未有过如此美妙的开局", ".\\Output.wav")
-	HZYS.playtext("污渍yyds")
+	HZYS.playText("污渍yyds")
 	
