4f276d5e0e1f27918d79093d9efab40874068748
DSP-8192 <zwh162476@163.com>
2022-08-25 03:02:50 +0100

    重构了部分代码，为以后的更新做准备

--- HZYS_GUI.py
+++ HZYS_GUI.py
@@ -1,21 +1,15 @@
-import json
 import ctypes
-from tkinter import Tk, Button, messagebox, scrolledtext
+from tkinter import Checkbutton, Tk, Button, messagebox, scrolledtext, filedialog, BooleanVar
 from huoZiYinShua import *
 
-#打开设置文件
-configFile= open(".\\settings.json", encoding="utf8")
-settings = json.load(configFile)
-configFile.close()
-
-#读取设置
-outputFile = settings["outputFile"]		#输出文件 (wav)
 
 #新建活字印刷类实例
-HZYS = huoZiYinShua(settings)
+HZYS = huoZiYinShua(".\\settings.json")
+
 
 
 #主框架
+#-------------------------------------------
 topFrame = Tk()
 #设置窗口大小
 topFrame.geometry("480x320")
@@ -27,27 +21,60 @@ topFrame.tk.call('tk', 'scaling', ScaleFactor/75)
 #窗口标题
 topFrame.title("欧炫！纯纯的活字印刷")
 
-#文本框
-textArea = scrolledtext.ScrolledText(topFrame, width=50, height=15)
 
+
+#动作
+#-------------------------------------------
 #直接播放的监听事件
 def onDirectPlay():
-    textToRead = textArea.get(1.0, 'end')
-    HZYS.directPlay(textToRead)
+	textToRead = textArea.get(1.0, 'end')
+	HZYS.directPlay(textToRead, inYsddMode=inYsddMode.get())
+
 
 #导出的监听事件
 def onExport():
-    textToRead = textArea.get(1.0, 'end')
-    HZYS.export(textToRead, outputFile)
-    messagebox.showinfo("疑似是成功了", "已导出到当前目录Output.wav下")
-    pass
+	textToRead = textArea.get(1.0, 'end')
+	outputFile = filedialog.asksaveasfilename(title="选择导出路径",
+											filetypes = (("wav音频文件", "*.wav*"),))
+	if(outputFile != ""):
+		if not outputFile.endswith(".wav"):
+			outputFile += ".wav"
+		HZYS.export(textToRead, filePath=outputFile, inYsddMode=inYsddMode.get())
+		messagebox.showinfo("疑似是成功了", "已导出到" + outputFile +"下")
+
+
+
+#生成选项
+#-------------------------------------------
+inYsddMode = BooleanVar()
+
+
+
+#GUI元素
+#-------------------------------------------
+#文本框
+textArea = scrolledtext.ScrolledText(topFrame, width=50, height=13)
+textArea.place(x=0, y=0)
+
+
+#复选框
+ysddCkBt = Checkbutton(topFrame, text="匹配到特定文字时使用原声大碟",
+						variable=inYsddMode, onvalue=True, offvalue=False)
+ysddCkBt.place(x=20, y=220)
+
 
 #按钮们
+#播放按钮
 playButton = Button(topFrame, text="直接播放", command=onDirectPlay, height=1, width=8)
 playButton.place(x=100, y=260)
+
+#导出按钮
 exportButton = Button(topFrame, text="导出", command=onExport, height=1, width=8)
 exportButton.place(x=280, y=260)
 
+
+
 #控件打包
+#-------------------------------------------
 textArea.pack()
 topFrame.mainloop()
--- huoZiYinShua.py
+++ huoZiYinShua.py
@@ -5,8 +5,9 @@
 from pydub import AudioSegment
 from pypinyin import lazy_pinyin
 import csv
+import json
 from pathlib import Path
-import winsound
+import playsound
 
 
 #文件路径转文件夹路径
@@ -18,15 +19,19 @@ def _fileName2FolderName(fileName):
 
 
 class huoZiYinShua:
-	def __init__(self, configuration):
-		dictFile = open(configuration["dictFile"], encoding="utf8")
-		ysddTableFile = open(configuration["ysddTableFile"], encoding="utf8")
+	def __init__(self, configFileLoc):
+		#读取设置文件
+		configFile = open(configFileLoc, encoding="utf8")
+		configuration = json.load(configFile)
+		configFile.close()
+
+		dictFile = open(configuration["dictFile"], encoding="utf8")			#读取单字词典 (csv)
+		ysddTableFile = open(configuration["ysddTableFile"], encoding="utf8")		#读取原声大碟文本与文件名对照表 (csv)
 
 		self.__voicePath = configuration["sourceDirectory"]					#单字音频文件存放目录
 		self.__ysddPath = configuration["ysddSourceDirectory"]				#原声大碟音频文件存放目录
 		self.__dictionary = dict(csv.reader(dictFile, delimiter=","))		#词典存放文件 (csv)
 		self.__ysddTable = dict(csv.reader(ysddTableFile, delimiter="\t"))	#原声大碟文本与文件名对照表 (csv)
-		self.__ysddMode = configuration["ysddModeEnabled"]					#是否启用原声大碟模式
 
 		self.__ysddTable = sorted(self.__ysddTable.items(), key=lambda x:len(x[0]), reverse=True)	#从长到短排序
 		self.__ysddTable = dict(self.__ysddTable)
@@ -34,23 +39,23 @@ class huoZiYinShua:
 
 	
 	#直接导出
-	def export(self, rawData, filePath):		
-		self.__concatenate(rawData)
+	def export(self, rawData, filePath=".\\Output.wav", inYsddMode=True):		
+		self.__concatenate(rawData, inYsddMode)
 		self.__export(filePath)
 		print("已导出到当前目录" + filePath + "下")
 	
 	
 	
 	#直接播放
-	def directPlay(self, rawData, tempPath = '.\HZYSTempOutput\\temp.wav'):
-		self.__concatenate(rawData)
+	def directPlay(self, rawData, tempPath=".\\HZYSTempOutput\\temp.wav", inYsddMode=True):
+		self.__concatenate(rawData, inYsddMode)
 		self.__export(tempPath)
-		winsound.PlaySound(tempPath, winsound.SND_FILENAME|winsound.SND_ASYNC)
+		playsound.playsound(tempPath)
 	
 	
 	
 	#生成中间文件
-	def __concatenate(self, rawData):
+	def __concatenate(self, rawData, inYsddMode):
 		missingPinyin = []
 		self.__concatenated = AudioSegment.empty()
 		
@@ -61,7 +66,7 @@ class huoZiYinShua:
 		#分割使用活字印刷的部分和使用原声大碟的部分
 		splitted = [[rawData, False]]		#[文本, 是否使用原声大碟]
 		#遍历要匹配的句子
-		if (self.__ysddMode):
+		if inYsddMode:
 			for ysdd in self.__ysddTable.items():
 				#遍历文本
 				i = -1
@@ -123,13 +128,9 @@ class huoZiYinShua:
 						missingPinyin.append(self.__ysddTable[pronunciations[i][0]])
 					self.__concatenated += AudioSegment.silent(duration = 250)
 
-
-				
 		#如果缺失拼音，则发出警告
 		if len(missingPinyin) != 0:
 			print("警告：缺失或未定义{}".format(missingPinyin))
-		
-		return missingPinyin
 		
 
 	
